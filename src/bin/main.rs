796177bb21dfa2913053457692fa0e3c5d9a2260
Hosshii <sao_heath6147.wistre@icloud.com>
2020-10-10 14:39:36 +0900

    fix

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -7,11 +7,10 @@ use std::time::Duration;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(1).unwrap();
 
-    for stream in listener.incoming().take(2) {
+    for stream in listener.incoming() {
         let stream = stream.unwrap();
-        let pool = ThreadPool::new(1).unwrap();
-
         pool.execute(|| {
             handle_connection(stream);
         });
